179edcc2b82bd0ee9ca7c0f30cb6c2502f527a6e
Zachary Nowicki <zacharynowicki@knights.ucf.edu>
2021-06-04 19:16:57 -0400

    Copyright

--- src/main/java/ex16.java
+++ src/main/java/ex16.java
@@ -24,7 +24,10 @@ Challenges
     If the user enters a number that’s less than zero or enters non-numeric data, display an error message that asks the user to enter a valid age.
     Instead of hard-coding the driving age in your program logic, research driving ages for various countries and create a lookup table for the driving ages and countries. Prompt for the age, and display which countries the user can legally drive in.
 */
-
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solutions
+ *  Copyright 2021 first_name last_name
+ */
 import java.util.Scanner;
 
 public class ex16 {
